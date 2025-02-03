a7b04a4ab24831bbe2992938d0e7c5fa1218356a
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2025-02-03 17:55:34 +0100

    setup project

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-import express from 'express';
 
-// const message = 'hello world';
-// console.log(message);
 
-const app = express();
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
 
-const PORT = 3000;
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
-
-app.use('*', (req, res, next)=>{
-    res.status(404).json({
-        message: 'Route not found',
-    });
-    });
-
-app.use((err, req, res, next)=>{
-res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-});
-});
-
-app.use((req, res, next) => {
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    next();
-  });
-
-app.listen(PORT, ()=>{
-console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
